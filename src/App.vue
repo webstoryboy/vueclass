91864b3632f06e3844596bf1e1c82738beb36998
webstoryboy <webstupids@gmail.com>
2022-10-26 13:14:04 +0900

    2022.10.26

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HeaderLayout fonts="nexon" />
+    <SliderSection fonts="nexon" />
+    <ImageSection attr="section nexon" layout="container" />
+    <ImgTextSection attr="section nexon gray" layout="container" />
+    <CardSection attr="section nexon" layout="container" />
+    <BannerSection fonts="gmark" />
+    <TextSection attr="section nexon" layout="container" />
+    <FooterLayout attr="section nexon gray" layout="container" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FooterLayout from "./components/layout/FooterLayout.vue";
+import HeaderLayout from "./components/layout/HeaderLayout.vue";
+import BannerSection from "./components/section/BannerSection.vue";
+import CardSection from "./components/section/CardSection.vue";
+import ImageSection from "./components/section/ImageSection.vue";
+import ImgTextSection from "./components/section/ImgTextSection.vue";
+import SliderSection from "./components/section/SliderSection.vue";
+import TextSection from "./components/section/TextSection.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderLayout,
+    FooterLayout,
+    SliderSection,
+    ImageSection,
+    ImgTextSection,
+    CardSection,
+    BannerSection,
+    TextSection,
+  },
+};
 </script>
-
-<style>
-
-</style>
